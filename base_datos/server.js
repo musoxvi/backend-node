d1fafaa4a043421555be5972db1c27b6ccbe4045
Manuel Uso <manuel.uso@extendeal.com>
2018-12-31 12:24:19 -0300

    configurando SQlite3

---
+++ base_datos/server.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3');
+console.log('hola')
+
+let db = new sqlite3.Database('proyecto-backend');
+
+db.run('CREATE TABLE task(id int AUTO_INCREMENT, description varchar(255))');
+
+
+db.close();
