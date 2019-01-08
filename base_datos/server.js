762374d20186051213327fe9fed46e802ff54503
Manuel Uso <manuel.uso@extendeal.com>
2019-01-07 22:04:17 -0300

    insercion de dato

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,8 +1,26 @@
+const  express = require('express');
 const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser'); //leer los campos que vienen en el cuerpo de la peticion
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 let db = new sqlite3.Database('proyecto-backend');
 
-db.run('CREATE TABLE task(id int AUTO_INCREMENT, description varchar(255))');
+//definiendo ruta
 
+app.post('', function(req, res){
+    db.run('INSER INTO tasks(description) VALUES("olakease")');
+    res.send('insercion finalizada');
+});
 
 db.close();
+
+app.listen(3000);
+
+process.on('SIGINT',function(){
+    console.log('Adios - Atte. El Servidor');
+    db.close();
+    process.exit();
+})
